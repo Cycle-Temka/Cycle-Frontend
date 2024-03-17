8210710cc3d121d5d41b7ed953b96ea4eb89a75e
serdeshnow <vserdes1977@mail.ru>
2024-03-17 20:01:24 +0300

    Create: Main

---
+++ app/src/widgets/Main.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "../styles/Main.css";
+
+function Main() {
+  return (
+    <main>
+      <div className="company-scroller"></div>
+      <div className="new-items"></div>
+      <div className="catlalog"></div>
+      <div className="promo"></div>
+      <div className="best-season-models"></div>
+      <div className="hill-bikes"></div>
+      <div className="advantages"></div>
+      <div className="equipment"></div>
+      <div className="reviews"></div>
+      <div className="contacts"></div>
+    </main>
+  );
+}
+
+export default Main;
